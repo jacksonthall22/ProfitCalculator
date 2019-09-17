bec669dd36a7ea38516232d667449da9b085f241
Jackson Hall <jacksonthall22@gmail.com>
2019-09-17 15:47:04 -0400

    Added getCsvReader function

--- profitcalculator.py
+++ profitcalculator.py
@@ -1,5 +1,23 @@
+import csv
+
+
 def main():
+    filename = input('Enter relative filepath for your CSV file:\n>>> ')
+    print(getCsvReader(filename))
+
+
+def calculateProfit(data):
     pass
+
+
+def getCsvReader(filename):
+    """Return a CSV reader for the given file (type = _csv.reader)."""
+
+    with open(filename) as file:
+        csv_reader = csv.reader(file, delimiter=',')
+
+    return csv_reader
+
 
 if __name__ == '__main__':
     main()
